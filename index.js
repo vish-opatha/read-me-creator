3daf8f119eae1fc97016f1652bbf2a155b4941f0
Vish O <v.opatha@gmail.com>
2021-08-09 23:18:35 +0930

    Update writeToFile function

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-// TODO: Include packages needed for this application
+// Packages/ modules needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const utils = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const markDown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = ["What is your Github username?", "What is your email address?", "What is your project name?",
+                   "Please write a short description of your project?", "What kind of license should your project have?",
+                   "What command should be run to install your dependencies?", "What command should be run to test?",
+                   "What does the user need to know about using the repository?", "What does the user need to know about contributing the repo?"];
+
+const[userName, userEmail, projectName, projectDescripton, licence, installDependencies, runTest, useRepo, contributeRepo ] = questions;
+
+inquirer
+  .prompt([
+    { type: 'input', name: 'uName', message: userName,},
+    { type: 'input', name: 'uEmail', message: userEmail,},
+    { type: 'input', name: 'proName', message: projectName,},
+    { type: 'input', name: 'proDecription', message: projectDescripton,},
+    { type: 'list',  name: 'proLicence', message: licence, choices: ['MIT', 'Apache 2.0', 'GPL 3.0','BSD 3','None'],},
+    { type: 'input', name: 'proDependencies', message: installDependencies,},
+    { type: 'input', name: 'proTest', message: runTest},
+    { type: 'input', name: 'proRepo', message: useRepo,},
+    { type: 'input', name: 'proContribution', message: contributeRepo,}
+
+])
+.then((answers) => {
+  // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+    const filename = "README.txt";
+    var data = answers.uName;
+    writeToFile(filename,data);
+  // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    // err ? console.log(err) : console.log(data);
+    // console.log(data);
+});
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) 
+{
+  const markDownText = generateMarkdown(data);
+  fs.writeFile(fileName,markDownText ,(err)=>
+  err ? console.log(err) : console.log('Successfully created index.html!'))
+};
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,6 +12,29 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let markDown = ``# ${data.title}
+
+  `
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+      <p class="lead">I am from ${answers.location}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
   return `# ${data.title}
 
 `;
